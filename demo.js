c72aa599ea044fc7709febad9ae4a0de094548e2
zzz-y <43123826+zzz-y@users.noreply.github.com>
2018-10-08 19:07:26 +0800

    Create demo.js

---
+++ demo.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+
+var fs = require('fs')
+
+var dirName = process.argv[2] // 你传的参数是从第 2 个开始的
+
+if(fs.existsSync(dirName)){
+	console.log('error: '+dirName+' 已经存在')
+	process.exit(1)
+} else {
+	fs.mkdirSync('./' + dirName) // mkdir $1
+	process.chdir('./' + dirName) // cd $1
+	fs.mkdirSync('css') // mkdir css
+	fs.mkdirSync('js') // mkdir js
+
+	fs.writeFileSync('./index.html', '<!DOCTYPE>\n<title>Hello</title>\n<h1>Hi</h1>')
+	fs.writeFileSync('./css/style.css', 'h1{color: red;}')
+	fs.writeFileSync('./js/main.js', 'var string = "Hello World"\nalert(string)')
+	console.log('success')
+	
+	process.exit(0)
+}
